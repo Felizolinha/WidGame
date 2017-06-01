b08c4088180a4c725fae94080018b76ba098e2af
Matheus Felizola <matheusbsb2009@hotmail.com>
2017-06-01 17:53:34 -0300

    Added framework project.

---
+++ WidGame/WidGame/WidGame.h
@@ -0,0 +1,19 @@
+//
+//  WidGame.h
+//  WidGame
+//
+//  Created by Matheus Felizola Freires on 01/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WidGame.
+FOUNDATION_EXPORT double WidGameVersionNumber;
+
+//! Project version string for WidGame.
+FOUNDATION_EXPORT const unsigned char WidGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WidGame/PublicHeader.h>
+
+
